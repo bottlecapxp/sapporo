4612f5a4690feb7b777461e5d74b086b2b0e5584
Christine Banlawi <christine.banlawi@gmail.com>
2021-08-07 17:39:57 -0400

    Updated home.js

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Pages.css'
+import Win from '../images/win.svg'
 
 /**
 * @author Decory Herbert
@@ -9,7 +10,12 @@ import './Pages.css'
 const Home = (props) => {
   return(
     <div className='home_container'> 
-    <h1>Home</h1>
+      <img src={Win} width="200" alt="win a home music studio"/>
+      <p>ARE YOU OVER 19 YEARS OF AGE?</p>
+      <div className="buttons">
+        <button>YES, LET ME IN</button>
+        <button>NO, TAKE ME HOME</button>
+      </div>
     </div>
    )
 
